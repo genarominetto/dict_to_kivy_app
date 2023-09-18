6303f3230f9e75f766046829a0408b672c57caa5
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 03:43:28 +0200

    Update create_screen.py

--- modules/create_screen.py
+++ modules/create_screen.py
@@ -1,4 +1,13 @@
 import os
+import re
+
+def sanitize_function_name(name):
+    """
+    Sanitizes the function name by replacing spaces with underscores, making all characters lowercase, and removing all characters that are not letters or numbers.
+    """
+    sanitized_name = name.replace(' ', '_').lower()
+    sanitized_name = re.sub(r'[^a-zA-Z0-9_]', '', sanitized_name)
+    return sanitized_name
 
 def create_screen(key_screen, reachable_screens, custom_functions=[], directory='screens/'):
     """
@@ -22,7 +31,8 @@ def create_screen(key_screen, reachable_screens, custom_functions=[], directory=
 
     # Add custom function imports
     for func in custom_functions:
-        class_definition += f"from utils.{key_screen}.{func} import {func}\n"
+        sanitized_func = sanitize_function_name(func)
+        class_definition += f"from utils.{key_screen}.{sanitized_func} import {sanitized_func}\n"
     
     class_definition += f"""
 class {key_screen}Screen(Screen):
@@ -39,7 +49,8 @@ class {key_screen}Screen(Screen):
     # Generate custom function placeholders
     custom_methods = []
     for func in custom_functions:
-        custom_method = f"""    {func} = staticmethod({func})"""
+        sanitized_func = sanitize_function_name(func)
+        custom_method = f"""    {sanitized_func} = staticmethod({sanitized_func})"""
         custom_methods.append(custom_method)
 
     # Combine the class definition and methods
