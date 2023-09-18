1ff64e094ec36721a00096736c339a09ba82b2f9
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 03:46:59 +0200

    Update create_main.py

--- modules/create_main.py
+++ modules/create_main.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 def create_main(screen_dict, title_height, transition='FadeTransition'):
     directory = 'app/'
@@ -13,14 +12,6 @@ from kivy.uix.screenmanager import ScreenManager, {transition}
 
     kv_loading_lines = "# Load KV files\n" + "\n".join([f"Builder.load_file('kv/{key}.kv')" for key in screen_dict.keys()])
     import_lines = "\n".join([f"from screens.{key} import {key}Screen" for key in screen_dict.keys()])
-    
-    # Direct import of utility functions
-    import_statements = []
-    for screen_name, attributes in screen_dict.items():
-        for function_name in attributes.get('custom_functions', []):
-            sanitized_function_name = re.sub(r'[^a-zA-Z0-9_]', '', function_name.replace(' ', '_')).lower()
-            import_statements.append(f"from utils.{screen_name}.{sanitized_function_name} import {sanitized_function_name}")
-    import_code = "\n".join(import_statements)
 
     screen_manager_code = f"""class MainApp(App):
 
@@ -36,7 +27,7 @@ from kivy.uix.screenmanager import ScreenManager, {transition}
 if __name__ == '__main__':
     MainApp().run()"""
 
-    main_py_content = f"{header}\n{kv_loading_lines}\n\n{import_lines}\n\n{import_code}\n\n{screen_manager_code}\n{screen_addition_lines}\n{set_current_line}\n{footer}"
+    main_py_content = f"{header}\n{kv_loading_lines}\n\n{import_lines}\n\n{screen_manager_code}\n{screen_addition_lines}\n{set_current_line}\n{footer}"
     output_file_path = os.path.join(directory, 'main.py')
     
     with open(output_file_path, 'w') as f:
