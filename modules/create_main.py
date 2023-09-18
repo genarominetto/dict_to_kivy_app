c7b42a489d408828f9792bc5ee50fbfd16f57cb9
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 03:02:48 +0200

    Update create_main.py

--- modules/create_main.py
+++ modules/create_main.py
@@ -1,6 +1,5 @@
 import os
 import re
-import importlib
 
 def create_main(screen_dict, title_height, transition='FadeTransition'):
     directory = 'app/'
@@ -13,34 +12,22 @@ from kivy.uix.screenmanager import ScreenManager, {transition}
 """
 
     kv_loading_lines = "# Load KV files\n" + "\n".join([f"Builder.load_file('kv/{key}.kv')" for key in screen_dict.keys()])
-
     import_lines = "\n".join([f"from screens.{key} import {key}Screen" for key in screen_dict.keys()])
-
-    # Generate import statements for custom utility functions
-    import_code = """try:
-    import importlib
-except ImportError:
-    pass  # Handle ImportError if necessary"""
-
-    import_statements = [
-        f"    importlib.import_module('utils.{screen_name}.{function_name}')"
-        for screen_name, attributes in screen_dict.items()
-        for function_name in attributes.get('functions', [])
-    ]
-    import_code += "\n" + "\n".join(import_statements)
+    
+    # Direct import of utility functions
+    import_statements = []
+    for screen_name, attributes in screen_dict.items():
+        for function_name in attributes.get('custom_functions', []):
+            sanitized_function_name = re.sub(r'[^a-zA-Z0-9_]', '', function_name.replace(' ', '_')).lower()
+            import_statements.append(f"from utils.{screen_name}.{sanitized_function_name} import {sanitized_function_name}")
+    import_code = "\n".join(import_statements)
 
     screen_manager_code = f"""class MainApp(App):
 
     def build(self):
         sm = ScreenManager(transition={transition}())"""
 
-    screen_addition_lines = []
-    for key in screen_dict.keys():
-        screen_creation = f"        {key}_screen = {key}Screen(name='{key}')"
-        screen_addition = f"        sm.add_widget({key}_screen)"
-        screen_addition_lines.extend([screen_creation, screen_addition])
-    screen_addition_lines = "\n".join(screen_addition_lines)
-
+    screen_addition_lines = "\n".join([f"        {key}_screen = {key}Screen(name='{key}')\n        sm.add_widget({key}_screen)" for key in screen_dict.keys()])
     first_screen = list(screen_dict.keys())[0]
     set_current_line = f"        sm.current = '{first_screen}'"
 
@@ -49,10 +36,9 @@ except ImportError:
 if __name__ == '__main__':
     MainApp().run()"""
 
-    # Combine all the parts to form the full main.py content
     main_py_content = f"{header}\n{kv_loading_lines}\n\n{import_lines}\n\n{import_code}\n\n{screen_manager_code}\n{screen_addition_lines}\n{set_current_line}\n{footer}"
-
     output_file_path = os.path.join(directory, 'main.py')
+    
     with open(output_file_path, 'w') as f:
         f.write(main_py_content)
 
