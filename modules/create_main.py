230a3f35949ef86d9cd0156e8a771ee6d6682d99
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 02:50:30 +0200

    Update create_main.py

--- modules/create_main.py
+++ modules/create_main.py
@@ -1,43 +1,39 @@
 import os
+import re
+import importlib
 
-# Function to generate the main.py file based on the given screens and their attributes
 def create_main(screen_dict, title_height, transition='FadeTransition'):
-    """
-    Generates the main.py file based on a given dictionary of screens and their attributes.
-
-    Parameters:
-    - screen_dict (dict): A dictionary where the key is the name of the key screen,
-                          and the value is another dictionary containing all the data for the screen,
-                          including reachable_screens and customization parameters.
-    - transition (str): The type of transition to use between screens.
-
-    Returns:
-    - str: The path to the generated main.py file.
-    """
-    # Create the directory if it doesn't exist
     directory = 'app/'
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    # Initialize parts of the main.py file
     header = f"""from kivy.lang import Builder
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, {transition}
 """
 
-    # Generate KV file loading lines with a preceding comment
     kv_loading_lines = "# Load KV files\n" + "\n".join([f"Builder.load_file('kv/{key}.kv')" for key in screen_dict.keys()])
 
-    # Generate import lines for Python classes
     import_lines = "\n".join([f"from screens.{key} import {key}Screen" for key in screen_dict.keys()])
 
-    # Generate ScreenManager code
+    # Generate import statements for custom utility functions
+    import_code = """try:
+    import importlib
+except ImportError:
+    pass  # Handle ImportError if necessary"""
+
+    import_statements = [
+        f"    importlib.import_module('utils.{screen_name}.{function_name}')"
+        for screen_name, attributes in screen_dict.items()
+        for function_name in attributes.get('functions', [])
+    ]
+    import_code += "\n" + "\n".join(import_statements)
+
     screen_manager_code = f"""class MainApp(App):
 
     def build(self):
         sm = ScreenManager(transition={transition}())"""
 
-    # Generate screen addition lines
     screen_addition_lines = []
     for key in screen_dict.keys():
         screen_creation = f"        {key}_screen = {key}Screen(name='{key}')"
@@ -45,25 +41,19 @@ from kivy.uix.screenmanager import ScreenManager, {transition}
         screen_addition_lines.extend([screen_creation, screen_addition])
     screen_addition_lines = "\n".join(screen_addition_lines)
 
-    # Add a line to set the current screen (default to the first screen in the dictionary)
     first_screen = list(screen_dict.keys())[0]
     set_current_line = f"        sm.current = '{first_screen}'"
 
-    # Footer
     footer = """        return sm
 
 if __name__ == '__main__':
     MainApp().run()"""
 
-    # Combine all parts to form the full main.py content
-    main_py_content = f"{header}\n{kv_loading_lines}\n\n{import_lines}\n\n{screen_manager_code}\n{screen_addition_lines}\n{set_current_line}\n{footer}"
+    # Combine all the parts to form the full main.py content
+    main_py_content = f"{header}\n{kv_loading_lines}\n\n{import_lines}\n\n{import_code}\n\n{screen_manager_code}\n{screen_addition_lines}\n{set_current_line}\n{footer}"
 
-    # Write the generated content to main.py
     output_file_path = os.path.join(directory, 'main.py')
     with open(output_file_path, 'w') as f:
         f.write(main_py_content)
 
     return output_file_path
-
-# Test the function with the sample dictionary
-#generate_main_py_file(sample_dict)
