1a9235bf059528a016f0d16950c43f6637c36391
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 20:54:19 +0200

    Update create_kv.py

--- modules/create_kv.py
+++ modules/create_kv.py
@@ -13,7 +13,7 @@ def create_kv(key_screen, reachable_screens, custom_functions,
     title_color = (adjust_color(color[0], adjust_amount), 
                    adjust_color(color[1], adjust_amount), 
                    adjust_color(color[2], adjust_amount))
-    
+
     text_color = (0, 0, 0) if avg_color > 0.5 else (1, 1, 1)
 
     directory = 'kv/'
@@ -34,6 +34,7 @@ def create_kv(key_screen, reachable_screens, custom_functions,
             size_hint_y: None
             height: self.minimum_height
             padding: 10
+            spacing: 10
             Label:
                 text: '{key_screen.capitalize()}'
                 color: {text_color}
@@ -49,7 +50,14 @@ def create_kv(key_screen, reachable_screens, custom_functions,
                 cols: {grid_columns}
                 size_hint_y: None
                 height: self.minimum_height
-                padding: 10"""
+                padding: 10
+                spacing: 10
+                canvas.before:
+                    Color:
+                        rgb: 0.5, 0.5, 0.5
+                    Rectangle:
+                        pos: self.pos
+                        size: self.size"""
 
     buttons = []
     for screen in reachable_screens:
@@ -59,7 +67,20 @@ def create_kv(key_screen, reachable_screens, custom_functions,
                     height: {button_height}
                     on_press: root.go_to_{screen}()"""
         buttons.append(button)
-        
+
+    custom_functions_grid = f"""            GridLayout:
+                cols: {grid_columns}
+                size_hint_y: None
+                height: self.minimum_height
+                padding: 10
+                spacing: 10
+                canvas.before:
+                    Color:
+                        rgb: {title_color[0]}, {title_color[1]}, {title_color[2]}
+                    Rectangle:
+                        pos: self.pos
+                        size: self.size"""
+
     custom_function_buttons = []
     for function_text in custom_functions:
         function_name = re.sub('[^0-9a-zA-Z_]', '', function_text.replace(' ', '_')).lower()
@@ -70,7 +91,7 @@ def create_kv(key_screen, reachable_screens, custom_functions,
                     on_press: root.{function_name}()"""
         custom_function_buttons.append(custom_button)
 
-    full_kv_content = "\n".join([kv_content] + buttons + custom_function_buttons)
+    full_kv_content = "\n".join([kv_content] + buttons + [custom_functions_grid] + custom_function_buttons)
 
     output_file_path = os.path.join(directory, f"{key_screen}.kv")
     with open(output_file_path, 'w') as f:
