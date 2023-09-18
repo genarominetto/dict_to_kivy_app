73dfcf3a8e0d621b43d415be5c40645fc05eec26
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-09-18 03:12:04 +0200

    Update create_screen.py

--- modules/create_screen.py
+++ modules/create_screen.py
@@ -1,22 +1,22 @@
 import os
 
-def create_screen(key_screen, reachable_screens, directory='screens/'):
+def create_screen(key_screen, reachable_screens, custom_functions=[], directory='screens/'):
     """
-    Generates a Python (.py) file that defines a screen class with methods to navigate to other screens.
+    Generates a Python (.py) file that defines a screen class with methods to navigate to other screens and custom functions.
 
     Parameters:
     - key_screen (str): The name of the key screen.
     - reachable_screens (list): A list of strings representing the names of screens that can be reached from the key screen.
+    - custom_functions (list): A list of strings representing the names of custom functions for the screen.
     - directory (str): The directory in which to save the Python file.
 
     Returns:
     - str: The path to the generated Python file.
     """
-    # Create the directory if it doesn't exist
+
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    # Generate the class definition without capitalizing the left part of the screen class name
     class_definition = f"""from kivy.uix.screenmanager import Screen
 
 class {key_screen}Screen(Screen):
@@ -26,14 +26,22 @@ class {key_screen}Screen(Screen):
     methods = []
     for screen in reachable_screens:
         method = f"""    def go_to_{screen}(self):
-        print('Navigating from Screen {key_screen} to Screen {screen}')
+        print('Navigating from {key_screen} to {screen}')
         self.manager.current = '{screen}'"""
         methods.append(method)
 
-    # Combine the class definition and methods
-    full_class_definition = class_definition + "\n".join(methods)
+    # Generate custom function placeholders
+    custom_methods = []
+    for func in custom_functions:
+        custom_method = f"""    def {func}(self):
+        print('Executing custom function {func}')
+        # Your code for custom function {func}"""
+        custom_methods.append(custom_method)
 
-    # Write the generated text to the Python file
+    # Combine the class definition and methods
+    full_class_definition = class_definition + "\n".join(methods) + "\n" + "\n".join(custom_methods)
+
+    # Write to the Python file
     output_file_path = os.path.join(directory, f"{key_screen}.py")
     with open(output_file_path, 'w') as f:
         f.write(full_class_definition)
